e449b0cf4f9bf45139e263e9ad075e21f55d7817
wilson Alves <wilson.f.alves@gmail.com>
2017-11-15 00:25:56 -0200

    from

--- resources/views/rota/teste2.blade.php
+++ resources/views/rota/teste2.blade.php
@@ -400,9 +400,9 @@
     <div class="container wow fadeInUp">
       <div class="row">
         <div class="col-md-12">
-          <h3 class="section-title">Contact Us</h3>
+          <h3 class="section-title">Contate Nos</h3>
           <div class="section-title-divider"></div>
-          <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+          <p class="section-description">Cadastre-se e teremos uma solução</p>
         </div>
       </div>
 

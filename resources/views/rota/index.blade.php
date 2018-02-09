8db4bcbe2543149cbd5ab5c32223b2c1d5560a32
wilson Alves <wilson.f.alves@gmail.com>
2018-02-09 00:38:54 -0200

    schema1

--- resources/views/rota/index.blade.php
+++ resources/views/rota/index.blade.php
@@ -17,12 +17,14 @@
         </div>
       </div>
         <div class="row">
+            <div itemscope itemtype ="http://schema.org/VideoObject">
               <div class="col-md-6">
                   <div class="embed-responsive embed-responsive-16by9 ">
                       
                       <iframe class="embed-responsive-item"  src="https://www.youtube.com/embed/rSd-VWQGkvk" frameborder="0" allowfullscreen></iframe>
                   </div>
-              </div>                          
+              </div> 
+            </div>                         
               <div class="col-md-6">  
                   <div class="embed-responsive embed-responsive-16by9">
                       
